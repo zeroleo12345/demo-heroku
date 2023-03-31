22e84a58ae06b652996db096fce6506285d92a66
zeroleo12345 <zeroleo12345@163.com>
2023-03-31 22:48:10 +0800

    harcode DIR_TMP

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,8 @@
 # Global variables
 DIR_CONFIG="/etc/v2ray"
 DIR_RUNTIME="/usr/bin"
-DIR_TMP="$(mktemp -d)"
+DIR_TMP="/tmp/v2ray_install"
+mkdir ${DIR_TMP}
 
 # Write V2Ray configuration
 cat << EOF > ${DIR_TMP}/heroku.json
@@ -31,7 +32,7 @@ cat << EOF > ${DIR_TMP}/heroku.json
 EOF
 
 # Get V2Ray executable release
-curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -o ${DIR_TMP}/v2ray_dist.zip
+curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/v2fly/v2ray-core/releases/download/v4.45.0/v2ray-linux-64.zip -o ${DIR_TMP}/v2ray_dist.zip
 busybox unzip ${DIR_TMP}/v2ray_dist.zip -d ${DIR_TMP}
 
 # Convert to protobuf format configuration
